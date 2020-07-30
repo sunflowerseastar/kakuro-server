c247d45da785662f6f5bba6f1994e909c588a0e2
Grant Surlyn <grant@sunflowerseastar.com>
2020-07-29 22:36:51 -0700

    fix

--- src/cross_sums_2/core.clj
+++ src/cross_sums_2/core.clj
@@ -25,6 +25,8 @@
 (def f6 (->flags '([:d 1 0 6 3] [:d 2 0 17 3] [:d 3 0 22 3]
                    [:r 0 1 15 3] [:r 0 2 13 3] [:r 0 3 17 3])))
 
+;; should return nothing since it needs to duplicate numbers
+(def f7-x (->flags '([:d 1 0 3 2] [:d 2 0 5 2] [:r 0 1 4 2] [:r 0 2 4 2])))
 
 (defn flags->flags-down [flags]
   (filter #(= (:direction %) :down) flags))
@@ -106,4 +108,5 @@
       (l/everyg is-single-digit all-lvars)
       (l/everyg adds-up rights)
       (l/everyg adds-up downs)
-      (fd/distinct all-lvars))))
+      (l/everyg #(fd/distinct (:lvars %)) rights)
+      (l/everyg #(fd/distinct (:lvars %)) downs))))
