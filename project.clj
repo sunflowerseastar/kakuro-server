3cbdaa2231a1d430795fd280b6f61135f14d13d5
Grant Surlyn <grant@sunflowerseastar.com>
2020-08-04 20:59:02 -0700

    add dep clojail

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[compojure "1.6.1"]
+  :dependencies [[clojail "1.0.6"]
+                 [compojure "1.6.1"]
                  [criterium "0.4.5"]
                  [incanter "1.9.3"]
                  [metosin/muuntaja "0.6.7"]
